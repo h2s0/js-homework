17569d1e053d052b64ef37772c5e386a90440640
h2s0 <h2s0_@naver.com>
2023-07-23 20:26:41 +0900

    일단 작동하는 코드

--- mission02/mission02/client/js/main.js
+++ mission02/mission02/client/js/main.js
@@ -11,41 +11,53 @@
 
 
 const nav = getNode('.nav');
+const ul = getNode('.ul');
 
-function handleSlider(e){
+
+// nav 를 클릭하면 작동되는 함수 (= 일어나는 일)
+function clickImage(e){
 
     e.preventDefault();
+    // target : e라는 객체 목록에서 내가 정확히 클릭한 것을 가져온 것의 가장 가까운 부모요소인 li, 안에 is-active를 넣어주려고 찾아옴
     const target = e.target.closest('li');
+    const button = e.target.closest('button');
 
-    if(!target){
-        return;
-    }
+    // 만약에 타겟이 없으면, 함수 실행을 중단한다.
+    if(!target || !button) return;
 
+    // list 는 이벤트가 걸려있는 곳 : ul 의 자식요소들이 배열로 존재하는 것
     const list = [... e.currentTarget.children];
-
     
-    list.forEach( li => removeClass(li,'is-active'));
+    // list 를 forEach 로 조회할건데, 조회할 대상은 li이고, removeClass 를 할 것이다.
+    list.forEach(li => removeClass(li, 'is-active'))
 
     addClass(target,'is-active')
 
+    const visualImg = getNode('.visual img');
+    const index = attr(target, 'data-index');
+    const nickName = getNode('.nickName');
+
     
 
-    console.log(list);
+    // 세 번째 인자에 들어가야할 것 : class 이름이 data-index 인 것의 자식요소인 이미지 / alt 텍스트
+    attr(visualImg,'src',`./assets/${data[index-1].src}`);
+    attr(nickName,'alt',data[index-1].alt);
+
+    // textContent 사용하여 닉네임 값을 바꿔줌 닉네임 class 안에 있는 것을 data 에서 name 가져와서..
+    nickName.textContent = data[index-1].name
+
+    css("body", "background", `linear-gradient(to bottom,${data[index-1].color[0]},#000`);
+
+    // ${data[index-1].color[0]} colorA변수
+    // #000 colorB 변수 로 지정해주기 나중에..
 
 }
 
 
 
 
-
-
-
-
-nav.addEventListener('click',handleSlider);
-
-
-
-
+// nav 를 '클릭' 하면 handleSlider 라는 함수가 작동됨
+ul.addEventListener('click',clickImage);
 
 
 
